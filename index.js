a284070f23f5ea02c89837ff9f9677ca2a00c891
Isaac Borbon <borbonisaac@hotmail.com>
2022-07-05 18:52:52 -0700

    new CORS

--- index.js
+++ index.js
@@ -29,11 +29,20 @@ app.get('/products/:id', cors(corsOptionsDelegate), function (req, res, next) {
 });
 
 app.use('/', cors(corsOptionsDelegate)); */
-app.all('/', function(req, res, next) {
-  res.header("Access-Control-Allow-Origin", "*");
-  res.header("Access-Control-Allow-Headers", "X-Requested-With");
-  next()
-});
+var allowedOrigins = ['https://weeatcliente.onrender.com/', 'https://weeat-restaurantes.onrender.com/'];
+app.use(cors({
+  origin: function(origin, callback){
+    // allow requests with no origin 
+    // (like mobile apps or curl requests)
+    if(!origin) return callback(null, true);
+    if(allowedOrigins.indexOf(origin) === -1){
+      var msg = 'The CORS policy for this site does not ' +
+                'allow access from the specified Origin.';
+      return callback(new Error(msg), false);
+    }
+    return callback(null, true);
+  }
+}));
 
 app.set('view engine', 'ejs');
 app.use(helmet());
